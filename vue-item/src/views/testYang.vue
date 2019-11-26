df0cda0353e1c896d2560661a0b896b4de081c87
yanghailong <opat5l@163.com>
2019-11-26 14:40:18 +0800

    testYANG

---
+++ vue-item/src/views/testYang.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:"test"
+}
+</script>
+
+<style lang="less" scoped>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+</style>
