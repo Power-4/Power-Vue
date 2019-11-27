d14ff6c732f600e9d1d15c39ddc459047f0cd91c
yanghailong <opat5l@163.com>
2019-11-27 16:03:36 +0800

    消缺页面

--- vue-item/src/views/Repair.vue
+++ vue-item/src/views/Repair.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="app-repair">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="消缺任务制定与分配" name="first">
+        <div class="zhiding">
 
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="消缺任务执行与回执" name="second">
+
+      </el-tab-pane>
+      <el-tab-pane label="消缺查询" name="third">
+
+      </el-tab-pane>
+      
+    </el-tabs>
   </div>
 </template>
 
 <script>
 export default {
-  name:"repair"
-}
+  name: "repair",
+  data() {
+      return {
+        activeName: 'first'
+      };
+    },
+    
+};
 </script>
 
 <style lang="less" scoped>
-  .app-repair {
-    width: 1000px;
-    height: 600px;
-    float: right;
-  }
+ul,
+li {
+  list-style: none;
+}
+
+.app-repair {
+  width: 1000px;
+  height: 600px;
+  float: right;
+}
+
+.zhiding {
+  width: 998px;
+  height: 300px;
+  border: 1px solid #000;
+}
 </style>
