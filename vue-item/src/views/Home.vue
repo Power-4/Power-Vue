2624e687a9aa726a39f915535297f2106e2c6374
jzxccm <c961086741@163.com>
2019-12-02 08:35:58 +0800

    home，work，页面细节修改

--- vue-item/src/views/Home.vue
+++ vue-item/src/views/Home.vue
@@ -72,7 +72,7 @@ export default {
     // 退出登录
     quit() {
       // 弹出确认窗口
-      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
+      this.$confirm("是否退出?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning"
